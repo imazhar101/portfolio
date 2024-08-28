df8548f6a09d13edcb561ebed3e3af17e5f49c39
Azharuddin <azhar@oneorigin.us>
2024-08-27 21:02:24 -0700

    main: Host pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/portfolio",
   output: "export",
   distDir: "pages",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
